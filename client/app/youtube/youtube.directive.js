83eeeb796b5e5e8ff1fffc9ce717d440aa2b7459
Ben Diuguid <Bendiuguid@gmail.com>
2016-02-09 12:57:17 -0500

    almost done with YouTube Feature

--- client/app/youtube/youtube.directive.js
+++ client/app/youtube/youtube.directive.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('fmgApp')
-  .directive('youtube', function ($window) {
+  .directive('youtube', function ($window, $http) {
     return {
-      templateUrl: 'app/youtube/youtube.html',
-      restrict: 'EA',
+      template: '<div id="youtube-player"></div>',
+      restrict: 'E',
+      scope: {
+        height: "@",
+        width: "@",
+        videoid: "@"
+      },
       link: function (scope, element, attrs) {
-
 
         // This code loads the IFrame Player API code asynchronously.
         var tag = document.createElement('script');
@@ -16,36 +20,51 @@ angular.module('fmgApp')
         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
         // This function creates an <iframe> (and YouTube player) after the API code downloads.
-        var player;
+        var player, videoIds;
         $window.onYouTubeIframeAPIReady = function() {
+
           player = new YT.Player('youtube-player', {
-            height: '390',
-            width: '640',
-            videoId: 'hoaPygaUw7E',
+            height: scope.height,
+            width: scope.width,
             events: {
               'onReady': onPlayerReady,
               'onStateChange': onPlayerStateChange
             }
           });
-        }
 
-        // The API will call this function when the video player is ready.
+          const part = 'snippet';
+          const channelId = 'UCBWT4QoCrNRuH54uhL4-B3Q';
+          const maxResults = '10';
+          const order = 'date';
+          const type = 'video';
+          const apiKey = 'AIzaSyCDhdYEHiYIz59b9bLGqaQg0Sdw7r35REE';
+          const urlString = `https://www.googleapis.com/youtube/v3/search?part=${part}&channelId=${channelId}&maxResults=${maxResults}&order=${order}&type=${type}&key=${apiKey}`;
+
+          $http.get(urlString)
+                .then((response) => {
+
+                  const videoIds = response.data.items.map((item)=> {
+                    return item.id.videoId;
+                  });
+
+                })
+                .catch((err) => {
+                  console.log('Error trying to search YouTube');
+                  console.log(err);
+                });
+        };
+
         function onPlayerReady(event) {
-          event.target.playVideo();
+          console.log('Player Ready');
+          event.target.loadPlaylist({
+            list: videoIds
+          });
         }
 
-        // 5. The API calls this function when the player's state changes. The function indicates that when playing a video (state=1), the player should play for six seconds and then stop.
-        var done = false;
         function onPlayerStateChange(event) {
-          if (event.data == YT.PlayerState.PLAYING && !done) {
-            setTimeout(stopVideo, 6000);
-            done = true;
-          }
-        }
-        function stopVideo() {
-          player.stopVideo();
+          console.log(`Player State Changed to: ${event.data}`);
         }
 
-      }
+      } // End of link function
     };
   });
