eb5cdc12faefcdb0ddd7e929ee0ac0e563d1eeaf
Gavin Scheele <gdscheele@gmail.com>
2016-02-22 12:50:57 -0500

    added toast notifications for text subscriptions.

--- client/app/signupheader/signupheader.controller.js
+++ client/app/signupheader/signupheader.controller.js
@@ -8,25 +8,28 @@
 
   class SignUpHeaderController {
 
-    constructor(SignUp) {
+    constructor(SignUp, toaster) {
       this.SignUp = SignUp;
       this.phoneNumber = undefined;
+      this.toaster = toaster;
     }
 
 
     createContact() {
       var SignUp = this.SignUp;
       var phoneNumber = this.phoneNumber;
+      var toaster = this.toaster;
+
       SignUp.createContact(phoneNumber).success(function(data) {
         console.log(data);
 
         if(!data.Response.Errors) {
           SignUp.sendConfirmationMessage(phoneNumber).success(function (data) {
-            alert("You've subscribed. A confirmation message has been sent to your phone.")
+            toaster.pop("success", "Congratulations!", "You have been subscribed. You should receive a confirmation text message shortly.");
           });
         }
         else if(data.Response.Errors[0] == "PhoneNumber: This phone number is already in your contacts list.") {
-          alert("You\'ve already registered");
+          toaster.pop("error", "Oops", "You have already subscribed to receive text updates from Jarmone.");
         }
 
       });
