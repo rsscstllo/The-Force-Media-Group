6057f306e63fa98a2bf15a92bb94705d8ef7404a
Kyle Brost <kjbrost@gmail.com>
2016-03-16 16:50:50 -0400

    added backend hooks to post to/delete from/edit items in database; added variable blog post limit; added code to switch between views

--- client/app/blog/blog.controller.js
+++ client/app/blog/blog.controller.js
@@ -1,6 +1,66 @@
 'use strict';
 
 angular.module('fmgApp')
-  .controller('BlogCtrl', function ($scope) {
+  .controller('BlogCtrl', function ($scope, $http) {
+    $scope.blogs = [];
+    $scope.limit = 5;
     $scope.message = 'Hello';
+    $scope.viewAll = true;
+    $scope.edit = false;
+    $scope.isLoggedIn = false;
+    $scope.isAdmin = false;
+
+    $http.get('api/blogs').then(function(response) {
+        console.log(response);
+        $scope.blogs = response.data;
+    });
+
+    $scope.updateBlogs = function() {
+        $http.get('api/blogs').then(function(response) {
+            console.log(response);
+            $scope.blogs = response.data;
+        });
+    }
+    $scope.incrementLimit = function() {
+        $scope.limit+=5;
+    }
+    $scope.editPost = function() {
+        $scope.edit = !$scope.edit;
+    };
+    $scope.submitEdit= function() {
+        $scope.currentBlog.updatedAt = moment().format('MMMM Do YYYY h:mm:ss a');
+        $http.put('api/blogs/'+$scope.currentBlog._id, $scope.currentBlog).then(function(response) {
+            console.log(response);
+            $scope.updateBlogs();
+            $scope.edit = false;
+        });
+    }
+    $scope.deletePost = function(blog) {
+        $http.delete('api/blogs/'+blog._id).then(function(response) {
+            console.log(response);
+            $scope.updateBlogs();
+        });
+    };
+    $scope.toggleView = function(blog) {
+        $scope.currentBlog = blog;
+        $scope.viewAll = ! $scope.viewAll;
+    }
+    $scope.resetView = function() {
+        $scope.viewAll = true;
+        $scope.edit = false;
+    }
+    $scope.saveDraft = function() {
+        console.log("save draft")
+    }
+    $scope.createPost = function() {
+        $scope.blogpost.published = true;
+        $scope.blogpost.createdAt = moment().format('MMMM Do YYYY h:mm:ss a');
+        $scope.blogpost.updatedAt = moment().format('MMMM Do YYYY h:mm:ss a');
+        $http.post('/api/blogs', $scope.blogpost).then(function(response) {
+            $scope.blogpost.title="";
+            $scope.blogpost.body="";
+            console.log(response);
+            $scope.updateBlogs();
+        });
+    }
   });
