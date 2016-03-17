21817bf5b1f4e1d0db864271b3b69826dfeab544
LARA DEDIC <laradedic@ufl.edu>
2016-03-17 13:39:23 -0400

    beginning of blog comments

--- client/app/store/store.controller.js
+++ client/app/store/store.controller.js
@@ -3,4 +3,36 @@
 angular.module('fmgApp')
   .controller('StoreCtrl', function ($scope) {
     $scope.message = 'Hello';
+    $scope.items=[
+      {
+        Name: 'item1',
+        Picture: '../assets/images/store/Merchandise_Icon.png',
+        Price: 19.99,
+        Description: 'description'
+      },
+      {
+        Name: 'item2',
+        Picture: '../assets/images/store/Merchandise_Icon.png',
+        Price: 19.99,
+        Description: 'description'
+      },
+      {
+        Name: 'item3',
+        Picture: '../assets/images/store/Merchandise_Icon.png',
+        Price: 19.99,
+        Description: 'description'
+      },
+      {
+        Name: 'item4',
+        Picture: '../assets/images/store/Merchandise_Icon.png',
+        Price: 19.99,
+        Description: 'description'
+      },
+      {
+        Name: 'item5',
+        Picture: '../assets/images/store/Merchandise_Icon.png',
+        Price: 19.99,
+        Description: 'description'
+      }
+    ];
   });
---
+++ server/api/blog/blog.controller.js
@@ -0,0 +1,103 @@
+/**
+ * Using Rails-like standard naming convention for endpoints.
+ * GET     /api/blogs              ->  index
+ * POST    /api/blogs              ->  create
+ * GET     /api/blogs/:id          ->  show
+ * PUT     /api/blogs/:id          ->  update
+ * DELETE  /api/blogs/:id          ->  destroy
+ */
+
+'use strict';
+
+import _ from 'lodash';
+import Blog from './blog.model';
+
+function respondWithResult(res, statusCode) {
+  statusCode = statusCode || 200;
+  return function(entity) {
+    if (entity) {
+      res.status(statusCode).json(entity);
+    }
+  };
+}
+
+function saveUpdates(updates) {
+  return function(entity) {
+    var updated = _.merge(entity, updates);
+    return updated.saveAsync()
+      .spread(updated => {
+        return updated;
+      });
+  };
+}
+
+function removeEntity(res) {
+  return function(entity) {
+    if (entity) {
+      return entity.removeAsync()
+        .then(() => {
+          res.status(204).end();
+        });
+    }
+  };
+}
+
+function handleEntityNotFound(res) {
+  return function(entity) {
+    if (!entity) {
+      res.status(404).end();
+      return null;
+    }
+    return entity;
+  };
+}
+
+function handleError(res, statusCode) {
+  statusCode = statusCode || 500;
+  return function(err) {
+    res.status(statusCode).send(err);
+  };
+}
+
+// Gets a list of Blogs
+export function index(req, res) {
+  Blog.findAsync()
+    .then(respondWithResult(res))
+    .catch(handleError(res));
+}
+
+// Gets a single Blog from the DB
+export function show(req, res) {
+  Blog.findByIdAsync(req.params.id)
+    .then(handleEntityNotFound(res))
+    .then(respondWithResult(res))
+    .catch(handleError(res));
+}
+
+// Creates a new Blog in the DB
+export function create(req, res) {
+  Blog.createAsync(req.body)
+    .then(respondWithResult(res, 201))
+    //save comments/usernames
+    .catch(handleError(res));
+}
+
+// Updates an existing Blog in the DB
+export function update(req, res) {
+  if (req.body._id) {
+    delete req.body._id;
+  }
+  Blog.findByIdAsync(req.params.id)
+    .then(handleEntityNotFound(res))
+    .then(saveUpdates(req.body))
+    .then(respondWithResult(res))
+    .catch(handleError(res));
+}
+
+// Deletes a Blog from the DB
+export function destroy(req, res) {
+  Blog.findByIdAsync(req.params.id)
+    .then(handleEntityNotFound(res))
+    .then(removeEntity(res))
+    .catch(handleError(res));
+}
