c70f88d166e17a5c491be0d814962dba979e73ac
rsscstllo <rsscstllo@gmail.com>
2016-03-18 17:06:23 -0400

    changing custom color theme panel in live time working

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -3,9 +3,27 @@
 (function() {
 
 class AdminController {
+
   constructor(User) {
     // Use the User $resource to fetch all users
     this.users = User.query();
+
+    this.customColor1 = "";
+    this.customColor2 = "";
+    this.customColor3 = "";
+    this.customColor4 = "";
+    this.customColor5 = "";
+  }
+
+  saveCustomColorTheme() {
+    console.log('save button clicked');
+    // this.customColor1 = customColor1;
+    // this.customColor2 = customColor2;
+    // this.customColor3 = customColor3;
+    // this.customColor4 = customColor4;
+    // this.customColor5 = customColor5;
+
+    console.log(this.customColor1);
   }
 
   delete(user) {
